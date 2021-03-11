137c292b6f68293eac3571f6872774dd8d85eeb8
briar9070 <lewis.briar.stevens@gmail.com>
2021-03-11 22:06:58 +0000

    mouse tracing working!

--- javaScript.js
+++ javaScript.js
@@ -2,13 +2,23 @@
 const container = document.querySelector("#container");
 
 const gridSection = document.createElement(`div`);
-gridSection.textContent = "Becky is Cute";
-gridSection.classList.add(`grid`);
+gridSection.textContent = "";
+gridSection.classList.add("grid");
+//gridSection.setAttribute("id", "grid");
 let i = 1;
 
-while (i <= 64) {
+while (i <= 256) {
 
     container.appendChild(gridSection.cloneNode(true));
+    gridSection.setAttribute("id", `section-${i}`);
     i++;
+}
+
+gridList = document.querySelectorAll(".grid");
+for (var j = 0; j < gridList.length; j++) {
+  gridList[j].addEventListener("mouseenter", function (e) {
+      console.log(e.target);
+      e.target.style.backgroundColor = 'red';
+  });
 }
 
